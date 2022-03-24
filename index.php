ec1a109460c1f4fe28984731b7d8e031e1ce8006
Rizal Hidayat <rizalhidayat180499@gmail.com>
2022-03-24 19:29:51 +0700

    example send button

--- index.php
+++ index.php
@@ -22,6 +22,69 @@
                 $result = curl_exec($ch);
                 curl_close($ch);
             }
+            if ($data->content == "!button1") {
+                $buttons = array(
+                    (object)[
+                        "buttonId" => "id1", "buttonText" => (object)["displayText" => "button 1", "type" => 1]
+                    ]
+                );
+                $button_message = [
+                    "text" => "hello zuwinda 🥳",
+                    "footer" => "zuwinda button",
+                    "buttons" => $buttons,
+                    "headerType" => 1
+                ];
+                $ch = curl_init( $base_url );
+                $payload = json_encode( array( "instances_id" => $data->instances_id, "to" => $data->from, "content" => $button_message ) );
+                curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
+                curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'x-access-key:' . $zuwinda_token));
+                curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+                $result = curl_exec($ch);
+                curl_close($ch);
+            }
+            if ($data->content == "!button2") {
+                $buttons = array(
+                    (object)[
+                        "index" => 1, "urlButton" => (object)["displayText" => "zuwinda", "url" => "https://zuwinda.com"]
+                    ],
+                    (object)[
+                        "index" => 2, "callButton" => (object)["displayText" => "call me", "url" => "+6285156172902"]
+                    ]
+                );
+                $button_message = [
+                    "text" => "hello zuwinda 🥳",
+                    "footer" => "zuwinda button 2",
+                    "templateButtons" => $buttons,
+                ];
+                $ch = curl_init( $base_url );
+                $payload = json_encode( array( "instances_id" => $data->instances_id, "to" => $data->from, "content" => $button_message ) );
+                curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
+                curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'x-access-key:' . $zuwinda_token));
+                curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+                $result = curl_exec($ch);
+                curl_close($ch);
+            }
+            if ($data->content == "!button3") {
+                $buttons = array(
+                    (object)[
+                        "title" => "Check Status", "rows" => array((object)["title" => "Healthy", "rowId" => "option1"], (object)["title" => "Love", "rowId" => "option2", "description" => "This is example description"])
+                    ],
+                );
+                $button_message = [
+                    "text" => "hello zuwinda 🥳",
+                    "footer" => "zuwinda button 3",
+                    "title" => "Title Example",
+                    "buttonText" => "Required, text on the button to view the list",
+                    "sections" => $buttons,
+                ];
+                $ch = curl_init( $base_url );
+                $payload = json_encode( array( "instances_id" => $data->instances_id, "to" => $data->from, "content" => $button_message ) );
+                curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
+                curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'x-access-key:' . $zuwinda_token));
+                curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+                $result = curl_exec($ch);
+                curl_close($ch);
+            }
         }
     }
 ?>
